8fbd4873f75e53693d582e72f6ff8e01377f85f5
f2065 <f2065@mail.ru>
2019-03-30 14:39:04 +0300

    Bluetooth: set BT-MAC via btmacaddr on boot

--- rootdir/system/etc/init.qcom.bt.sh
+++ rootdir/system/etc/init.qcom.bt.sh
@@ -20,12 +20,25 @@ failed ()
   exit $2
 }
 
+# Set Bluetooth MAC address using btnvtool on boot
+if [ -n "${1+x}" ] && [ "$1" = "btmacaddr" ]; then
+  # Ensure .bt_nv.bin exists
+  btnvtool -O
+  # Convert '1.2a.3b.4c.5d.6d' to '6d:5d:4c:3b:2a:01'
+  btaddr=$(btnvtool -p 2>&1 | \
+    awk '/--board-address/ {split($2, mac, "."); for (i=1;i<=6;i++) {if (length(mac[i]) == 1) mac[i] = "0" mac[i]; }; printf("%s:%s:%s:%s:%s:%s\n", mac[6], mac[5], mac[4], mac[3], mac[2], mac[1]);} ')
+  setprop ro.boot.btmacaddr ${btaddr}
+  logi "setprop ro.boot.btmacaddr ${btaddr}"
+  exit 0
+fi
+
 # Note that "hci_qcomm_init -e" prints expressions to set the shell variables
 # BTS_DEVICE, BTS_TYPE, BTS_BAUD, and BTS_ADDRESS.
 
 POWER_CLASS=`getprop qcom.bt.dev_power_class`
 TRANSPORT=`getprop ro.qualcomm.bt.hci_transport`
 BDADDR=`cat /data/misc/bluetooth/bdaddr`
+BTMACADDR=`getprop ro.boot.btmacaddr`
 
 #find the transport type
 logi "Transport : $TRANSPORT"
@@ -33,6 +46,7 @@ logi "Transport : $TRANSPORT"
 setprop bluetooth.status off
 
 logi "BDADDR: $BDADDR"
+logi "ro.boot.btmacaddr: $BTMACADDR"
 
 case $POWER_CLASS in
   1) PWR_CLASS="-p 0" ;
